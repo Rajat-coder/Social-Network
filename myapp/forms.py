83dbdd0d3c6ff1f9af6f8346fcf1b47cbb37bf1f
Rajat Handa <you@example.com>
2021-02-20 19:02:10 +0530

    changes

---
+++ myapp/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+class signupform(forms.ModelForm):
+    username = forms.CharField(label="Username", max_length=100)
+    email = forms.CharField(label="Email", max_length=100)
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+
+    def clean_password2(self):
+        pass1 = self.cleaned_data.get("password1")
+        pass2 = self.cleaned_data.get("password2")
+        if pass1 and pass2 and pass1 != pass2:
+            raise forms.ValidationError("Password does not match or not enterted properly")
+        return pass2
+
+    def save(self, commit=True):
+        userobj = super(signupform, self).save(commit=False)
+        userobj.set_password(self.cleaned_data["password2"])
+
+        if commit:
+            userobj.save()
+        return userobj
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView
+from myapp.forms import *
+from django.urls import reverse_lazy, reverse
+
 
 # Create your views here.
+
+class signuppage(SuccessMessageMixin,CreateView):
+    form_class=signupform
+    template_name='signup.html'
+    success_message = "Your account has been created successfully"
+    success_url = reverse_lazy('registerpage')
+
+
+    def dispatch(self, *args, **kwargs):
+        return super(signuppage,self).dispatch(*args, **kwargs)
