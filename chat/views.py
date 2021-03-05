4f30a6ab1642a1fc9ea3b301fd7cf7728de671d3
Rajat Handa <you@example.com>
2021-03-05 22:31:48 +0530

    added views in chat appc

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
 
-# Create your views here.
+class ThreadView(View):
+    template_name = 'chat/chat.html'
+
+
+    def get_queryset(self):
+        return Thread.objects.by_user(self.request.user)
+
+    def get_object(self):
+        other_username  = self.kwargs.get("username")
+        self.other_user = get_user_model().objects.get(username=other_username)
+        obj = Thread.objects.get_or_create_personal_thread(self.request.user, self.other_user)
+        if obj == None:
+            raise Http404
+        return obj
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['me'] = self.request.user
+        context['thread'] = self.get_object()
+        context['user'] = self.other_user
+        context['messages'] = self.get_object().message_set.all()
+        return context
+
+    def get(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, **kwargs):
+        self.object = self.get_object()
+        thread = self.get_object()
+        data = request.POST
+        user = request.user
+        text = data.get("message")
+        Message.objects.create(sender=user, thread=thread, text=text)
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context=context)
