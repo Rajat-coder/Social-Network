135327f7a07f1664774a5c39966c34bb6ed6069c
Rajat Handa <handarajat111@gmail.com>
2021-04-18 23:52:58 +0530

    changes

---
+++ shared/admin/__init__.py
@@ -0,0 +1,12 @@
+from shared.admin.sites import site
+from shared.admin.options import UserDetailsAdmin, DropdownAdmin, HomepageDetailsAdmin
+from shared.admin.actions import download_as_csv
+
+__all__ = [
+    'site',
+    'UserDetailsAdmin',
+    'DropdownAdmin',
+    'HomepageDetailsAdmin',
+    'download_as_csv',
+]
+
