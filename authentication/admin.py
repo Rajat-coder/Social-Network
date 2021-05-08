369986b09130f3648125e455a69a12fd5c0cd9f5
Rajat Handa <handarajat111@gmail.com>
2021-05-08 23:50:54 +0530

    New Start

---
+++ authentication/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered, site
+from django.apps import apps
+
+# Register your models here.
+
+for model in apps.get_app_config('authentication').get_models():
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered as e:
+        pass
--- myproject/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter,URLRouter
-from chat.consumers import EchoConsumer
-from django.urls import path
-
-application=ProtocolTypeRouter({
-    "websocket":URLRouter([
-        path('ws/chat/',EchoConsumer)
-    ])
-})
